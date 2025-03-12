d59bdce2446a019c24c51f056ed5c1b6bf3c927e
Paul Torres <ptorres@pipingrock.com>
2025-03-11 20:43:43 -0700

    Add new advanced functionalities to advanceMath.js

--- Modulo 1 - Modularidad/MathOperation/app.js
+++ Modulo 1 - Modularidad/MathOperation/app.js
@@ -1,14 +1,16 @@
 import { sum, resta, multiplicacion, division } from './utils/basicMath.js';
-import { square, cube, squareRoot, power } from './utils/advancedMath.js';
+import { square, cube, squareRoot, power, porcentaje, redondeo, modulo } from './utils/advancedMath.js';
 
-console.log(sum(10, 3));
-console.log(resta(10, 3));
-console.log(multiplicacion(10, 3));
-console.log(division(10, 3));
-
-console.log('=======================');
-
-console.log(square(10));
-console.log(cube(10));
-console.log(squareRoot(10));
-console.log(power(4, 0));
+console.log("Suma: ", sum(10, 3));
+console.log("Resta: ",resta(10, 3));
+console.log("Multiplicación: ",multiplicacion(10, 3));
+console.log("Division: ",division(10, 3));
+console.log("Cuadrado: ", square(10));
+console.log("Cubo: ", cube(10));
+console.log("Raiz Cuadrada", squareRoot(10));
+console.log("Potencia: ", power(4, 0));
+console.log("Porcentaje: ", porcentaje(100, 30));
+console.log("Redondeo: ", redondeo(4.56));
+console.log("Redondeo: ", redondeo(4.50));
+console.log("Redondeo: ", redondeo(4.34));
+console.log("Modulo: ", modulo(9, 3));
--- Modulo 1 - Modularidad/MathOperation/utils/advancedMath.js
+++ Modulo 1 - Modularidad/MathOperation/utils/advancedMath.js
@@ -1,16 +1,42 @@
-export function square(a){
-    return a**2;
+import { division } from "./basicMath.js";
+
+export function square(a) {
+    return a ** 2;
 }
 
-export function cube(a){
-    return a**3;
+export function cube(a) {
+    return a ** 3;
 }
 
-export function squareRoot(a){
-    return Math.sqrt(a);
+export function squareRoot(a) {
+    return a * a;
 }
 
-export function power(a, b = 2){
-    return a**b;
+export function power(a, b = 2) {
+    return a ** b;
 }
 
+export function porcentaje(a, b) {
+    return a * b / 100;
+}
+
+export function redondeo(a) {
+    let entero = Math.trunc(a);
+    let decimal = a - entero;
+    
+    if (decimal == 0.50) {
+        return a;
+    } else {
+        if (decimal < 0.50) {
+            return entero;
+        }
+        if (decimal > 0.50) {
+            return entero + 1;
+        }
+    }
+}
+
+export function modulo(a, b){
+    let resultado_division = division(a, b);
+    return a - Math.trunc(resultado_division) * b;
+}
